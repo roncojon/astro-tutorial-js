d38096a4a02b0259564a0d58dc8abf9a5bee7130
Ronald <rguillen@datamart.cl>
2024-06-03 01:45:20 -0400

    vercel zzzz

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   output: "server",
-  // adapter: vercel(),
   site: "https://astro-tutorial-js.vercel.app/"
 });
