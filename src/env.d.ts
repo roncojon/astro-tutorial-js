9fe8102dd99f8595a4a23563cc02ec83139157f3
Ronald <rguillen@datamart.cl>
2024-06-20 02:21:04 -0400

    ygu

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,20 +1,22 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-    readonly FIREBASE_PRIVATE_KEY_ID: string;
-    readonly FIREBASE_PRIVATE_KEY: string;
-    readonly FIREBASE_PROJECT_ID: string;
-    readonly FIREBASE_CLIENT_EMAIL: string;
-    readonly FIREBASE_CLIENT_ID: string;
-    readonly FIREBASE_AUTH_URI: string;
-    readonly FIREBASE_TOKEN_URI: string;
-    readonly FIREBASE_AUTH_CERT_URL: string
-    readonly FIREBASE_CLIENT_CERT_URL: string;
-    readonly FIREBASE_API_KEY: string;
-    readonly FIREBASE_AUTH_DOMAIN: string;
-    readonly FIREBASE_STORAGE_BUCKET: string;
-    readonly FIREBASE_MESSAGING_SENDER_ID: string;
-    readonly FIREBASE_APP_ID : string;
-    readonly FIREBASE_MEASUREMENT_ID: string;
+    // readonly FIREBASE_PRIVATE_KEY_ID: string;
+    // readonly FIREBASE_PRIVATE_KEY: string;
+    // readonly FIREBASE_PROJECT_ID: string;
+    // readonly FIREBASE_CLIENT_EMAIL: string;
+    // readonly FIREBASE_CLIENT_ID: string;
+    // readonly FIREBASE_AUTH_URI: string;
+    // readonly FIREBASE_TOKEN_URI: string;
+    // readonly FIREBASE_AUTH_CERT_URL: string
+    // readonly FIREBASE_CLIENT_CERT_URL: string;
+
+    // readonly PUBLIC_FIREBASE_API_KEY: string;
+    // readonly PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+    // readonly PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+    // readonly PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+    // readonly PUBLIC_FIREBASE_APP_ID : string;
+    // readonly FIREBASE_MEASUREMENT_ID: string;
+    // readonly PUBLIC_FIREBASE_PROJECT_ID: string;
   }
   
   interface ImportMeta {
