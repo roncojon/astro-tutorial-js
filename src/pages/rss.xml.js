e8c690b8f3084bbfb237f35ebddf1cf8f6fcb513
Ronald <rguillen@datamart.cl>
2024-06-24 02:29:42 -0400

    update rss.xml.js file according to posts in DB

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,33 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import {serverAllPosts} from "../firebase/database/serverAllPosts";
+import rss from '@astrojs/rss';
 
 export async function GET(context) {
+  const items = serverAllPosts.map(post => ({
+    title: post.title,
+    description: post.content.substring(0, 200), // Use a substring as a description
+    link: `/posts/${post.id}`,
+    pubDate: post?.createdAt?.toDateString() ?? "2024", // Assuming you have a createdAt field
+  }));
+
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: items,
     customData: `<language>en-us</language>`,
   });
 }
+
+/////// OLD (GETTING DATA FROM ACTUAL POSTS .MD FILES) ///// 
+// import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+
+// export async function GET(context) {
+//   return rss({
+//     title: 'Astro Learner | Blog',
+//     description: 'My journey learning Astro',
+//     site: context.site,
+//     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+//     customData: `<language>en-us</language>`,
+//   });
+// }
