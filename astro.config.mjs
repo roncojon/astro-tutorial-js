9a651cfe7a8d1c959346253227d1f21e50c3a708
Ronald <rguillen@datamart.cl>
2024-08-14 02:25:56 -0400

    -add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: vercel({
     edgeMiddleware: true
   }),
-  site: "https://irachile.com/", // "https://astro-tutorial-js.vercel.app/",
-  integrations: [react()]
+  site: "https://irachile.com/",
+  // "https://astro-tutorial-js.vercel.app/",
+  integrations: [react(), tailwind()]
 });
