ed87d54a57813522987a93bf18747a21062ca5ea
Ronald <rguillen@datamart.cl>
2024-09-26 12:29:21 -0400

    -remove vercel middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel({
+  adapter: vercel(/* {
     edgeMiddleware: true
-  }),
+  } */),
   site: "https://irachile.com/",
   // "https://astro-tutorial-js.vercel.app/",
   integrations: [react(), tailwind()]
